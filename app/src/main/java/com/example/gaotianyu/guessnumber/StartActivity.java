b95c5aa7d88f6d1b2ec3a85b0f3305632475ae9d
gty <513884454@qq.com>
2019-05-25 22:25:44 +0800

    ..

--- app/src/main/java/com/example/gaotianyu/guessnumber/StartActivity.java
+++ app/src/main/java/com/example/gaotianyu/guessnumber/StartActivity.java
@@ -2,9 +2,10 @@ package com.example.gaotianyu.guessnumber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class StartActivity extends AppCompatActivity {
-
+private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
